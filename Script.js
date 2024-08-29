467793424ea2c37db35255a4ff3e4ff3d55ca7eb
Nicola Major <nicola@major.travel>
2024-08-29 21:59:13 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+document.getElementById('danceButton').addEventListener('click', function() {
+    const beavercorn = document.getElementById('beavercorn');
+    beavercorn.style.display = 'block';
+    beavercorn.classList.add('dance');
+});
